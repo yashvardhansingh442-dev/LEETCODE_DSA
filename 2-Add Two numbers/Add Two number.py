310cb027f786b56f32d784146bb52c17547105a8
Yash Vardhan Singh <yashvardhansingh442@gmail.com>
2026-06-29 20:47:23 +0530

    Implement addTwoNumbers method in Solution class

---
+++ 2-Add Two numbers/Add Two number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        curr = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+
+            total = x + y + carry
+            carry = total // 10
+
+            curr.next = ListNode(total % 10)
+            curr = curr.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return dummy.next
