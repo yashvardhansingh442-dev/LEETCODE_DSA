4019883074a60e69f857f9b52c9d4b240dfcfef6
yash vardhan singh <yashvardhansingh442@gmail.com>
2026-07-09 21:12:04 +0530

    Implement function to convert sorted array to BST

---
+++ 108-converted sorted array to binary search tree/converted sorted array to bst.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortedArrayToBST(self, nums: list[int]) -> TreeNode:
+        def build(left, right):
+            if left > right:
+                return None
+            mid = (left + right) // 2
+            root = TreeNode(nums[mid])
+            root.left = build(left, mid - 1)
+            root.right = build(mid + 1, right)
+            return root
+        
+        return build(0, len(nums) - 1)
