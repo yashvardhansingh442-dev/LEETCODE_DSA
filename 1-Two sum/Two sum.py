6d70951c8de7d009ff94a7648ad434e29ab06907
yash vardhan singh <yashvardhansingh442@gmail.com>
2026-07-14 16:56:09 +0530

    Delete 1-Two sum directory

--- 1-Two sum/Two sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        seen = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], i]
-            seen[num] = i
-        return []  # unreachable per problem constraints, but keeps type checkers happy
